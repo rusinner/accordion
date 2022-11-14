6e2717d2a2850d4a25efd6e97f5b1ef9a425bbec
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-14 15:12:20 +0200

    css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React, { useState } from "react";
+import SingleQuestion from "./Question";
+import data from "./data";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [questions, setQuestions] = useState(data);
+  return (
+    <div className="App">
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Question.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, info }) => {
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn">btn</button>
+      </header>
+      <p>{info}</p>
+    </article>
+  );
+};
+export default Question;
